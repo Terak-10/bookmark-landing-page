5f00b968a9e6dad6811007c7235838e1ca13a43a
Terak-10 <jurajstefanik77@gmail.com>
2022-05-30 13:23:44 +0200

    progress

--- faq.js
+++ faq.js
@@ -12,21 +12,40 @@ const answer4 = document.querySelector(".faq-answer-4");
 
 const questionBox4 =  document.querySelector(".question-box-4");
 
+// Arrows
+const arrow1 = document.querySelector(".faq-question_arrow-1-blue");
+const arrow2 = document.querySelector(".faq-question_arrow-2-blue");
+const arrow3 = document.querySelector(".faq-question_arrow-3-blue");
+const arrow4 = document.querySelector(".faq-question_arrow-4-blue");
+
+const arrow1Orange = document.querySelector(".faq-question_arrow-1");
+const arrow2Orange = document.querySelector(".faq-question_arrow-2");
+const arrow3Orange = document.querySelector(".faq-question_arrow-3");
+const arrow4Orange = document.querySelector(".faq-question_arrow-4");
+
+
 
 question1.addEventListener("click", function() {
     answer1.classList.toggle("hidden");
+    arrow1.classList.toggle("hidden");
+    arrow1Orange.classList.toggle("hidden");
 });
 
 question2.addEventListener("click", function() {
     answer2.classList.toggle("hidden");
+    arrow2.classList.toggle("hidden");
+    arrow2Orange.classList.toggle("hidden");
 });
 
 question3.addEventListener("click", function() {
     answer3.classList.toggle("hidden");
+    arrow3.classList.toggle("hidden");
+    arrow3Orange.classList.toggle("hidden");
 });
 
 question4.addEventListener("click", function() {
     answer4.classList.toggle("hidden");
     questionBox4.classList.toggle("hidden");
+    arrow4.classList.toggle("hidden");
+    arrow4Orange.classList.toggle("hidden");
 });
-
